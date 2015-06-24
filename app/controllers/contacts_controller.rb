84c7dbaa65096fa56750cc46f9c830b1b960ae35
Alessio Pellegrino <apellegrino6@gmail.com>
2015-06-24 22:17:15 +0000

    added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,13 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     
     if @contact.save
+      # When form is submitted, query string parameterssets hashes and sends value to variables from parameter to email
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      comments = params[:contact][:comment]
+      #The following just says deliver the email once form is saved
+      ContactMailer.contact_email(name, email, body).deliver
+      
       flash[:success] = "Message sent."
       redirect_to new_contact_path
     else
